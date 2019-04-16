08d82b44f907af4f2e37737a8279f257d72a086f
Cassiano Kunsch <cassiano.kunsch@outlook.com>
2019-04-15 23:03:06 -0300

    Criando a rota de cliente

--- start/routes.js
+++ start/routes.js
@@ -1,17 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
 Route.get('/', () => ({ greeting: 'Hello world in JSON' }));
@@ -19,3 +5,5 @@ Route.get('/', () => ({ greeting: 'Hello world in JSON' }));
 Route.post('/sessions', 'SessionController.create');
 
 Route.post('/users', 'UserController.create');
+
+Route.resource('clientes', 'ClienteController').apiOnly();
