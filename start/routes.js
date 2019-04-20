7ae21e90cdbd2c09567f03343234597ddca21dcc
Cassiano Kunsch <cassiano.kunsch@outlook.com>
2019-04-20 13:11:13 -0300

    Alterando as migrations

--- start/routes.js
+++ start/routes.js
@@ -7,3 +7,8 @@ Route.post('/sessions', 'SessionController.create');
 Route.post('/users', 'UserController.create');
 
 Route.resource('clientes', 'ClienteController').apiOnly();
+Route.resource('formapagamentos', 'FormaPagamentoController').apiOnly();
+Route.resource('veiculos', 'VeiculoController').apiOnly();
+Route.resource('marcas', 'MarcaController').apiOnly();
+Route.resource('modelos', 'ModeloController').apiOnly();
+Route.resource('servicos', 'ServicoSController').apiOnly();
