ba100f481cf8e18aa1b0ea1bddd08db46fdf0046
Cassiano Kunsch <cassiano.kunsch@outlook.com>
2019-04-20 18:54:14 -0300

    Criando novas rotas de ordem de servico e servico itens e colocando o midware de autenticacao

--- start/routes.js
+++ start/routes.js
@@ -2,13 +2,30 @@ const Route = use('Route');
 
 Route.get('/', () => ({ greeting: 'Hello world in JSON' }));
 
-Route.post('/sessions', 'SessionController.create');
-
+Route.post('/login', 'LoginController.create');
 Route.post('/users', 'UserController.create');
 
-Route.resource('clientes', 'ClienteController').apiOnly();
-Route.resource('formapagamentos', 'FormaPagamentoController').apiOnly();
-Route.resource('veiculos', 'VeiculoController').apiOnly();
-Route.resource('marcas', 'MarcaController').apiOnly();
-Route.resource('modelos', 'ModeloController').apiOnly();
-Route.resource('servicos', 'ServicoSController').apiOnly();
+Route.resource('clientes', 'ClienteController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('formapagamentos', 'FormaPagamentoController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('veiculos', 'VeiculoController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('marcas', 'MarcaController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('modelos', 'ModeloController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('servicos', 'ServicoController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('ordemservicos', 'OrdemServicoController')
+  .apiOnly()
+  .middleware('auth');
+Route.resource('ordemservicoitens', 'OrdemServicoItenController')
+  .apiOnly()
+  .middleware('auth');
